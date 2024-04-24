2468d20103d35684c30956528d62e8ca0c9d5895
Valid Mohammadi <63536756+ValidMo@users.noreply.github.com>
2024-04-24 22:44:01 +0330

    Initial Commit

---
+++ QuickHabitTracker/QuickHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickHabitTrackerApp.swift
+//  QuickHabitTracker
+//
+//  Created by Valid Mohammadi on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickHabitTrackerWidget/QuickHabitTrackerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  QuickHabitTrackerWidgetBundle.swift
+//  QuickHabitTrackerWidget
+//
+//  Created by Valid Mohammadi on 24.04.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct QuickHabitTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        QuickHabitTrackerWidget()
+    }
+}
