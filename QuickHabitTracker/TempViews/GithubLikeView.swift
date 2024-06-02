27fb9e4e6a030460e1f46bf1c3d09723a4482d72
Valid Mohammadi <63536756+ValidMo@users.noreply.github.com>
2024-06-02 21:49:49 +0330

    - Made a temporary view to use in HomeView

---
+++ QuickHabitTracker/TempViews/GithubLikeView.swift
@@ -0,0 +1,18 @@
+//
+//  GithubLikeView.swift
+//  QuickHabitTracker
+//
+//  Created by Valid Mohammadi on 2.06.2024.
+//
+
+import SwiftUI
+
+struct GithubLikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GithubLikeView()
+}
---
+++ QuickHabitTracker/TempViews/UITestView3.swift
@@ -0,0 +1,18 @@
+//
+//  UITestView3.swift
+//  QuickHabitTracker
+//
+//  Created by Valid Mohammadi on 2.06.2024.
+//
+
+import SwiftUI
+
+struct UITestView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UITestView3()
+}
